31b1032c750af2de7be5faef39a5a3edb053036d
Andreas Olsson <photoguy.apo@gmail.com>
2017-01-04 21:46:03 +0100

    Add files via upload
    Cleaned it up.

--- main.rkt
+++ main.rkt
@@ -10,14 +10,10 @@
 (define (transpose lists) ; collumns to rows!
   (apply map list lists))
 
-;(transpose '((1 3 5) (2 4 6))) ; test
-
 (define (pmap func . lists) ; pmap
   (map touch
        (for/list ([a (transpose lists)])
          (future (lambda () (apply func a)))
          )))
 
-;(pmap (lambda (x)(car x)) '((a b)(c d)(e f))) ; a test
-;(pmap * '(1 2 3 4 5 6 7 8 9) '(1 2 3 4 5 6 7 8 9)) ; a nother test
-;(pmap (lambda (x y) (* x y)) '(1 2 3 4 5 6 7 8 9) '(1 2 3 4 5 6 7 8 9)) ; yet a nother test!
+
